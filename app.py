46f1d2da00a6669a66cb281209f3dc8a3a92a793
yuvraj-shahi-011 <yuvrajshahi2081@gmail.com>
2026-06-22 10:43:27 +0530

    Move TMDB API key to environment variables

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
 
 app = Flask(__name__)
 
+API_KEY = os.getenv("API_KEY")
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
+
+    response = requests.get(url)
+    data = response.json()
+    movies = data["results"]
+
+    return render_template("index.html", movies=movies)
 
 if __name__ == "__main__":
     app.run(debug=True)
