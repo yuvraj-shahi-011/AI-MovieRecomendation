8342230fd0b8ee70d20206182e3cf19874e52c0c
yuvraj-shahi-011 <yuvrajshahi2081@gmail.com>
2026-06-23 21:07:06 +0530

    Add navigation bar to homepage

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template
-from dotenv import load_dotenv
-import os
 import requests
-
-load_dotenv()
 
 app = Flask(__name__)
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = "YOUR_API_KEY"
 
 @app.route("/")
 def home():
-    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
 
-    response = requests.get(url)
-    data = response.json()
-    movies = data["results"]
+    trending_url = f"https://api.themoviedb.org/3/trending/movie/week?api_key={API_KEY}"
+    popular_url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
+    top_url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}"
+    upcoming_url = f"https://api.themoviedb.org/3/movie/upcoming?api_key={API_KEY}"
 
-    return render_template("index.html", movies=movies)
+    trending = requests.get(trending_url).json()["results"]
+    popular = requests.get(popular_url).json()["results"]
+    top_rated = requests.get(top_url).json()["results"]
+    upcoming = requests.get(upcoming_url).json()["results"]
+
+    return render_template(
+        "index.html",
+        trending=trending,
+        popular=popular,
+        top_rated=top_rated,
+        upcoming=upcoming
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
